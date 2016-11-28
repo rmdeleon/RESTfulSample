386f4e8f399007583f5b657817a576e0945b1efc
Raul de Leon <rmdeleon@gmail.com>
2016-11-27 21:19:46 -0500

    Adding Ignore Donuts option

--- src/demo.js
+++ src/demo.js
@@ -5,41 +5,58 @@ function getTransactions() {
 	xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.setRequestHeader('Accept', 'application/json');
 	xhr.onloadend = function() {
-		cleanData(JSON.parse(this.response));
+		try {
+			cleanData(JSON.parse(this.response));
+		} catch (e) {
+			document.getElementById('content').textContent = "error";
+		}
 	};
 	xhr.onerror = function(err) {
-	    document.getElementById('content').textContent = "eror";
+	    document.getElementById('content').textContent = "error";
 	};
 	args = {"args": {"uid":  1110590645, "token":  "61C950D69F43B5A24603B02307549DB2", "api-token":  "AppTokenForInterview", "json-strict-mode": false, "json-verbose-response": false}};
 	xhr.send(JSON.stringify(args));
+}
+
+function isValidTransaction(transaction) {
+	if (document.getElementById("ignoreDonuts").checked) {
+		var m = transaction.merchant; 
+		if (m == "Krispy Kreme Donuts" ||
+			m == "DUNKIN #336784") {
+			return false;
+		}
+	}
+	return true;
 }
 
 function cleanData(transData) {
 	var aggData = {}; // Aggregated data {"2014": {spent: 2, income: 3}, "2015": {spent:5, income: 4}, ...}
 	
 	transData.transactions.forEach(function(obj) {
-	      var date = new Date(obj['clear-date']), // parse transaction date
-	          dateLength = 0,
-	          amount = obj.amount/10000; //convert centocents to dollars
-	      
-	      //Calculate the length of the key depending on what is aggregated by
-	      if (document.getElementById('aggregateBy_day').checked) {
-	    	  dateLength = 10; // create month key with format YYYY-MM-DD
-	      } else if (document.getElementById('aggregateBy_month').checked) {
-	    	  dateLength = 7; // create month key with format YYYY-MM
-	      } else if (document.getElementById('aggregateBy_year').checked) {
-	    	  dateLength = 4 // create month key with format YYYY
-	      }
-	      var aggKey = date.toISOString().slice(0, dateLength); 
-	      
-	      // create element for current month if needed
-	      aggData[aggKey] = aggData[aggKey] || {'spent':0, "income":0}; 
-	      
-	      if (amount > 0) { //income
-	    	  aggData[aggKey].income += amount;
-	      } else { //spent
-	    	  aggData[aggKey].spent += amount;
-	      }
+		  if (isValidTransaction(obj)) {
+		      var date = new Date(obj['clear-date']), // parse transaction date
+		          dateLength = 0,
+		          amount = obj.amount/10000; //convert centocents to dollars
+		      
+		      //Calculate the length of the key depending on what is aggregated by
+		      if (document.getElementById('aggregateBy_day').checked) {
+		    	  dateLength = 10; // create month key with format YYYY-MM-DD
+		      } else if (document.getElementById('aggregateBy_month').checked) {
+		    	  dateLength = 7; // create month key with format YYYY-MM
+		      } else if (document.getElementById('aggregateBy_year').checked) {
+		    	  dateLength = 4 // create month key with format YYYY
+		      }
+		      var aggKey = date.toISOString().slice(0, dateLength); 
+		      
+		      // create element for current month if needed
+		      aggData[aggKey] = aggData[aggKey] || {'spent':0, "income":0}; 
+		      
+		      if (amount > 0) { //income
+		    	  aggData[aggKey].income += amount;
+		      } else { //spent
+		    	  aggData[aggKey].spent += amount;
+		      }
+		  }
 	  });
 	
 	// render the table
